a302a584747187be56f646d0f80dfe25558741c3
yatinnagpal <yatinnagpal2016@gmail.com>
2024-04-11 18:56:30 +0530

    add persistor in redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {persistReducer, persistStore} from 'redux-persist';
 import userReducer from './user/userSlice.js';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({user: userReducer})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: {user: persistedReducer},
 //   middleware: (getDefaultMiddleware) => {
 //     getDefaultMiddleware({
 //         serializableCheck: false,
 //     })
 //   }
 })
+
+export const persistor = persistStore(store);
